c3444ffdbc8bc317e9d13915ba34f7ae04c8bbcb
Yoon.07.09 <159544864+samhomesss@users.noreply.github.com>
2025-06-26 23:34:39 +0900

    [FEAT] Brightness

--- Assets/Scripts/GameSystem/FullScreen_Toggle/Brightness.cs
+++ Assets/Scripts/GameSystem/FullScreen_Toggle/Brightness.cs
@@ -1,11 +1,42 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Brightness : MonoBehaviour
 {
+    [SerializeField] Volume volume; // URP Volume containing ColorAdjustments
+
     Slider _slider;
+    ColorAdjustments _colorAdjustments;
+
     private void Start()
     {
         _slider = GetComponent<Slider>();
+
+        if (volume == null)
+        {
+            volume = FindObjectOfType<Volume>();
+        }
+
+        if (volume != null)
+        {
+            volume.profile.TryGet(out _colorAdjustments);
+
+            if (_colorAdjustments != null)
+            {
+                _slider.value = _colorAdjustments.postExposure.value;
+            }
+        }
+
+        _slider.onValueChanged.AddListener(SetBrightness);
+    }
+
+    void SetBrightness(float value)
+    {
+        if (_colorAdjustments != null)
+        {
+            _colorAdjustments.postExposure.value = value;
+        }
     }
 }
